c1dc8308cd5bd03a7e4118aa9e4dddb888e54ccb
yeison1ruano2 <yeisonstiven100@gmail.com>
2025-04-19 20:09:13 -0500

    Actualizacion de logger Json Service

--- src/main/java/com/softmarket/apisoftmarket/services/FacturaService.java
+++ src/main/java/com/softmarket/apisoftmarket/services/FacturaService.java
@@ -1,9 +1,10 @@
 package com.softmarket.apisoftmarket.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.softmarket.apisoftmarket.dto.FacturaDto;
 import com.softmarket.apisoftmarket.dto.FacturaRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface FacturaService {
-  ResponseEntity<FacturaDto> crearfactura(FacturaRequest facturaRequest);
+  ResponseEntity<FacturaDto> crearfactura(FacturaRequest facturaRequest) throws JsonProcessingException;
 }
--- src/main/java/com/softmarket/apisoftmarket/services/FacturaServiceImpl.java
+++ src/main/java/com/softmarket/apisoftmarket/services/FacturaServiceImpl.java
@@ -1,10 +1,15 @@
 package com.softmarket.apisoftmarket.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softmarket.apisoftmarket.controller.FacturaController;
 import com.softmarket.apisoftmarket.dto.FacturaDto;
 import com.softmarket.apisoftmarket.dto.FacturaRequest;
 import com.softmarket.apisoftmarket.entity.ExternalApiProperties;
 import com.softmarket.apisoftmarket.entity.FacturaResponse;
 import com.softmarket.apisoftmarket.entity.FactusTokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,20 +23,25 @@ public class FacturaServiceImpl implements FacturaService{
   private final WebClient.Builder webClientBuilder;
   private final ExternalApiProperties externalApiProperties;
   private final FacturaMapper facturaMapper;
+  private final ObjectMapper objectMapper;
+
+  private static final Logger logger = LoggerFactory.getLogger(FacturaServiceImpl.class);
 
   public FacturaServiceImpl(AuthenticationService authenticationService,
                             WebClient.Builder webClientBuilder,
                             ExternalApiProperties externalApiProperties,
-                            FacturaMapper facturaMapper) {
+                            FacturaMapper facturaMapper, ObjectMapper objectMapper) {
     this.authenticationService = authenticationService;
     this.webClientBuilder = webClientBuilder;
     this.externalApiProperties = externalApiProperties;
     this.facturaMapper = facturaMapper;
+    this.objectMapper = objectMapper;
   }
 
   @Override
-  public ResponseEntity<FacturaDto> crearfactura(FacturaRequest facturaRequest) {
+  public ResponseEntity<FacturaDto> crearfactura(FacturaRequest facturaRequest) throws JsonProcessingException {
     FactusTokenResponse factusTokenResponse = authenticationService.authenticationFactus();
+    logger.info("Token Response: {} ",objectMapper.writeValueAsString(factusTokenResponse));
     //Crear factura en Factus
     try {
       FacturaResponse responseFactus =
@@ -44,6 +54,7 @@ public class FacturaServiceImpl implements FacturaService{
                       .retrieve()
                       .bodyToMono(FacturaResponse.class)
                       .block();
+      logger.info("Factus Response: {} ",objectMapper.writeValueAsString(responseFactus));
       assert responseFactus != null;
       FacturaDto facturaDto = facturaMapper.responseFactusToDto(responseFactus);
       return ResponseEntity.ok(facturaDto);
