624c36a322ff6945d80f2bfab77500390b4cb5ca
yeison1ruano2 <yeisonstiven100@gmail.com>
2025-06-17 07:14:42 -0500

    deploy 17/06/2025

--- src/main/java/com/softmarket/apisoftmarket/services/FacturaService.java
+++ src/main/java/com/softmarket/apisoftmarket/services/FacturaService.java
@@ -3,7 +3,6 @@ package com.softmarket.apisoftmarket.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.softmarket.apisoftmarket.dto.FacturaDto;
 import com.softmarket.apisoftmarket.dto.FacturaRequest;
-import com.softmarket.apisoftmarket.entity.FacturaResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface FacturaService {
