25591a0a1bb2db359a04c711b1a7d8e829a80a51
yeison1ruano2 <yeisonstiven100@gmail.com>
2025-06-19 20:08:37 -0500

    Envio de mensaje JSON

--- src/main/java/com/softmarket/apisoftmarket/controller/GenericController.java
+++ src/main/java/com/softmarket/apisoftmarket/controller/GenericController.java
@@ -1,6 +1,7 @@
 package com.softmarket.apisoftmarket.controller;
 
 import com.softmarket.apisoftmarket.dto.GenericResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenericController {
 
   @GetMapping()
-  public ResponseEntity<GenericResponse> endpointBasura(){
-    return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse(HttpStatus.OK.value(),""));
+  public ResponseEntity<GenericResponse> endpointBasura(HttpServletRequest request){
+    String ip = request.getHeader("X-Forwarded-For");
+    if(ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)){
+      ip = request.getRemoteAddr();
+    }
+    String mensaje = "Peticion recibida desde la IP: " + ip;
+    return ResponseEntity.status(HttpStatus.OK).body(new GenericResponse(HttpStatus.OK.value(),mensaje));
   }
 }
